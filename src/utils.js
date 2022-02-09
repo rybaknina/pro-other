4b4427e1e82d14c7e1d90632476d70b5c18de776
Nina Rybak <nrybak@dev.vtb.ru>
2022-02-09 14:23:21 +0300

    task-3: add tests, change pow function

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,12 @@
 function pow(x, y) {
     if (isNaN(x) || isNaN(y)) return NaN;
     if (y === 0) return 1;
+    let k = y > 0 ? y : -y;
     let res = x;
-    for (let i = 1; i < y; i++) {
+    for (let i = 1; i < k; i++) {
         res *= x;
     }
-    return res;
+    return y > 0 ? res : +(1 / res);
 }
 
 module.exports = {pow};
--- test/utils.test.js
+++ test/utils.test.js
@@ -19,3 +19,19 @@ test('undefined в 2 степени = NaN', () => {
 test('NaN в 2 степени = NaN', () => {
     expect(pow(NaN, 2)).toBe(NaN);
 });
+
+test('-2 в 3 степени = 8', () => {
+    expect(pow(-2, 3)).toBe(-8);
+});
+
+test('-2 в 2 степени = 4', () => {
+    expect(pow(-2, 2)).toBe(4);
+});
+
+test('-2 в -2 степени = 0.25', () => {
+    expect(pow(-2, -2)).toBe(0.25);
+});
+
+test('-2 в -3 степени = -0.125', () => {
+    expect(pow(-2, -3)).toBe(-0.125);
+});
